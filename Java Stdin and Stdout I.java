0f297da1fc1d8692afea733e44d8dd844d41c5b5
sanjana-reddy1652 <81432794+sanjana-reddy1652@users.noreply.github.com>
2021-07-07 08:53:51 +0530

    Create Java Stdin and Stdout I.java

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,30 @@
+/* Java Stdin and Stdout I
+
+Sample Input
+
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+*/
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
